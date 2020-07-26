339ef1262fc789583e870ff4fafacaf88abbafc6
cban <cleoclerd@gmail.com>
2020-07-26 23:58:26 +0200

    setup the network layer with retrofit and its dependencies

--- app/src/main/java/com/potter/triwizard/injection/NetworkModule.kt
+++ app/src/main/java/com/potter/triwizard/injection/NetworkModule.kt
@@ -1,10 +1,54 @@
 package com.potter.triwizard.injection
 
+import com.potter.triwizard.BuildConfig
+import com.potter.triwizard.network.AuthInteceptor
+import com.potter.triwizard.network.TwizardApi
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.schedulers.Schedulers
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(ApplicationComponent::class)
-object  NetworkModule {
+object NetworkModule {
+
+    @Provides
+    fun provideBaseUrl() = BuildConfig.BASE_URL
+
+    @Provides
+    @Singleton
+    fun provideAuthInterceptorOkHttpClient(
+        authInterceptor: AuthInteceptor
+    ): OkHttpClient {
+        val okHttpClientBuilder = OkHttpClient.Builder()
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+
+        okHttpClientBuilder.addInterceptor(logging)
+        return okHttpClientBuilder.addInterceptor(authInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient, baseUrl: String): Retrofit? {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.createWithScheduler(Schedulers.io()))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit) = retrofit.create(TwizardApi::class.java)
 }
