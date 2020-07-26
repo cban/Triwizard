a979a5d6a4f68c5fc3d415fabe1a118a6ad82b84
cban <cleoclerd@gmail.com>
2020-07-26 18:41:50 +0200

    setup basic project structure with dagger hilt dependency injection

---
+++ app/src/main/java/com/potter/triwizard/TwizardApplication.kt
@@ -0,0 +1,8 @@
+package com.potter.triwizard
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TwizardApplication:Application()  {
+}
---
+++ app/src/main/java/com/potter/triwizard/injection/NetworkModule.kt
@@ -0,0 +1,10 @@
+package com.potter.triwizard.injection
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object  NetworkModule {
+}
