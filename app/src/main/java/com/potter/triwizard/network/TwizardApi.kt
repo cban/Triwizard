9846d214243fcc33b52c36d87f72f356ed988247
cban <cleoclerd@gmail.com>
2020-07-28 05:03:03 +0200

    implemented HouseDetailsFragment

--- app/src/main/java/com/potter/triwizard/network/TwizardApi.kt
+++ app/src/main/java/com/potter/triwizard/network/TwizardApi.kt
@@ -5,6 +5,7 @@ import io.reactivex.rxjava3.core.Flowable
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface TwizardApi {
 
@@ -17,8 +18,8 @@ interface TwizardApi {
     @GET("houses")
    suspend fun getHouses(): Response<List<House>>
 
-    @GET("houses/:house_id")
-    suspend fun getHouse(@Path("houses_id") houseId: String): Response<House>
+    @GET("houses/{id}")
+    suspend fun getHouse(@Path("id") houseId: String): Response<House>
 
     @GET("spells")
     suspend fun getSpells(): Response<List<Spell>>
---
+++ app/src/main/java/com/potter/triwizard/repository/SpellRepository.kt
@@ -0,0 +1,8 @@
+package com.potter.triwizard.repository
+
+import com.potter.triwizard.data.Spell
+import retrofit2.Response
+
+interface SpellRepository {
+    suspend  fun getHouses(): Response<List<Spell>>
+}
